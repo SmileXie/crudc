e500fefa4a4aba5c1ca8dc15faba815a028c48a4
smilexie <smilexie1113@gmail.com>
2022-02-20 16:12:09 +0800

    support payload name

--- crudc.py
+++ crudc.py
@@ -11,7 +11,7 @@ DIR_OUTPUT = 'output'
 
 NOT_REPLACE_FLAG = 'DONOT_REPLACE'
 
-def crudc_replace_names(module_name, struct_name, key_name):
+def crudc_replace_names(module_name, struct_name, key_name, payload_name):
     for input_path in glob.glob(DIR_TEMPLATE + r'/**/*', recursive=True):
         if not os.path.isfile(input_path):
             continue
@@ -24,16 +24,21 @@ def crudc_replace_names(module_name, struct_name, key_name):
                     line = line.replace('modulename', module_name) if module_name != NOT_REPLACE_FLAG else line 
                     line = line.replace('structname', struct_name) if struct_name != NOT_REPLACE_FLAG else line
                     line = line.replace('keyname', key_name) if key_name != NOT_REPLACE_FLAG else line
+                    line = line.replace('payloadname', payload_name) if payload_name != NOT_REPLACE_FLAG else line
                     write_fp.write(line)
 
 def crudc_print_help():
     print('CRUDC Help:')
-    print('    example: ./crudc.py --module=[modulename] --struct=[structname] --key=[keyname]')
+    print('    example: ./crudc.py --module=[modulename] --struct=[structname] --key=[keyname] --payload=[payloadname]')
     return
+
+def crudc_replace_struct(struct_name):
+    # regular exp: payloadname_s((.|\n)*)\};
+    pass
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], '-m:-s:-k:', ['module=', 'struct=', 'key='])
+        opts, args = getopt.getopt(sys.argv[1:], '-m:-s:-k:-p:', ['module=', 'struct=', 'key=', 'payload='])
     except getopt.GetoptError:
         crudc_print_help()
         return
@@ -48,6 +53,9 @@ def main():
         if opt_name in ('-k', '--key'):
             key_name = opt_value
             print("key_name=%s" % key_name)
+        if opt_name in ('-p', '--payload'):
+            payload_name = opt_value
+            print("payload_name=%s" % payload_name)
     
     if 'module_name' not in locals():
         module_name = NOT_REPLACE_FLAG
@@ -55,8 +63,10 @@ def main():
         struct_name = NOT_REPLACE_FLAG
     if 'key_name' not in locals():
         key_name = NOT_REPLACE_FLAG
+    if 'payload_name' not in locals():
+        payload_name = NOT_REPLACE_FLAG
 
-    crudc_replace_names(module_name, struct_name, key_name)
+    crudc_replace_names(module_name, struct_name, key_name, payload_name)
 
 
 
